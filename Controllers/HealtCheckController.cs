4cbf1fec2f2d8c67c09770a5f01785299c1d665b
MdGolamRabbaniRafi <dreamlessboyrafi7@gmail.com>
2025-06-27 11:51:13 +0600

    Add project files.

---
+++ Controllers/HealtCheckController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Backend.Controllers
+{
+    public class HealtCheckController : ApiController
+    {
+        // GET: HealtCheck
+        [HttpGet]
+        [Route("health/check")]
+        public IHttpActionResult Index()
+        {
+            return Json(new { message = "API is healthy" });
+        }
+    }
+}
