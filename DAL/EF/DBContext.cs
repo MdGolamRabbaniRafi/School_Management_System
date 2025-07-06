616c5c7c493614ccc3582016322ccc6a53821fae
MdGolamRabbaniRafi <dreamlessboyrafi7@gmail.com>
2025-07-06 15:11:25 +0600

    initial migration

---
+++ DAL/EF/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL.Models;
+
+
+namespace DAL.EF
+{
+    internal class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+    }
+}
---
+++ DAL/Migrations/202507060900213_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        FirstName = c.String(),
+                        Email = c.String(),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        Password = c.String(),
+                        BloodGroup = c.String(),
+                        UserType = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
